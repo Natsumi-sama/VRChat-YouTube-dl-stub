423631d2e9a15e8ddf62f94f876ce6fcb8e54d7c
Natsumi <cmcooper123@hotmail.com>
2021-10-13 22:29:25 +1300

    a

--- VRCYoutubedl Linux.php
+++ VRCYoutubedl Linux.php
@@ -1,5 +1,4 @@
 <?php
-chdir('./mog422'); //Directory containing videos
 if (!isset($_GET['url'])) {
     header("HTTP/1.0 404 Not Found");
     echo("404");
@@ -11,27 +10,23 @@ $newURL = "";
 preg_match("#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $url, $matches);
 if (substr($url, 0, 23) == "http://storage.llss.io/") {
     $file = substr($url, 23, strlen($url));
-} else if (substr($url, 0, 34) == "https://jd.pypy.moe/api/v1/videos/") {
-    $file = substr($url, 34, strlen($url));
+} else if (substr($url, 0, 33) == "http://jd.pypy.moe/api/v1/videos/") {
+    $file = substr($url, 33, strlen($url));
 } else if ($matches) {
     $file = "$matches[0].mp4";
 }
 if ($file) {
-    if (file_exists("$file")) {
-        $newURL = "http://localhost/$file";
-    } else {
-        $dirs = array_filter(glob("*"), 'is_dir');
-        foreach ($dirs as $dir) {
-            if (file_exists("$dir/$file")) {
-                $newURL = "http://localhost/$dir/$file";
-            }
+    $dirs = array_filter(glob("*"), 'is_dir');
+    foreach ($dirs as $dir) {
+        if (file_exists("$dir/$file")) {
+            $newURL = "https://qwertyuiop.nz/pypy/$dir/$file";
         }
     }
 }
 if ($newURL) {
     echo $newURL;
 } else {
-    $shell_output = shell_exec("/usr/bin/youtube-dl --no-playlist -f 'mp4[height<=?1080]/best[height<=?1080]' --get-url " . escapeshellarg($url) . " 2>&1");
+    $shell_output = shell_exec("yt-dlp -f 'b[ext=mp4]' --no-playlist --get-url " . escapeshellarg($url) . " 2>&1");
     if (substr($shell_output, 0, 9) == "WARNING: ") {
         echo $url;
     } else {
